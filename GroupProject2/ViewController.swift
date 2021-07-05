2151665a092fdc0fd1b36a1b8391be94ac9deb37
lado at luka <tsivtsivadzelado@yahoo.com>
2021-07-05 03:00:10 -0700

    Initial Commit

---
+++ GroupProject2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GroupProject2
+//
+//  Created by lado tsivtsivadze on 7/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
