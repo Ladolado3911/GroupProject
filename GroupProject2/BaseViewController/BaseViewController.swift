e203b09dbffc3abf11527f23eed86fc83f6b9ec6
Kotlina <57289866+Zuraba69@users.noreply.github.com>
2021-07-05 14:16:30 +0400

    - deleted some unused function

--- GroupProject2/BaseViewController/BaseViewController.swift
+++ GroupProject2/BaseViewController/BaseViewController.swift
@@ -3,10 +3,4 @@ import UIKit
 class BaseViewController: UIViewController, Storyboarded, CoordinatorDelegate{
     var coordinator: AppCoordinatorProtocol?
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        navigationController?.isNavigationBarHidden = true
-    }
-    
 }
